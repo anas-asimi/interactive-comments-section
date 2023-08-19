d44310a4be28757aa2a85b61b6dd02d77748ea84
Anas Asimi <anas2020cr7@gmail.com>
2023-08-19 18:54:07 +0100

    add postcss with following plugins: 	- autoprefixer 	- postcss-nesting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import postcssNesting from "postcss-nesting";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	css: {
+		postcss: {
+			plugins: [postcssNesting, autoprefixer],
+		},
+	},
+});
