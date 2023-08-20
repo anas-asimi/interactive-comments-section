5448b9865d1299b40ed5b70c4bda1eead10ce43b
Anas Asimi <anas2020cr7@gmail.com>
2023-08-20 19:55:27 +0100

    add nested replies feature with help of recursion (everything is just a function)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,37 +1,60 @@
 /* eslint-disable react/prop-types */
-function Comment({ score, content, user, createdAt, currentUserName }) {
+function Comment({
+	score,
+	content,
+	user,
+	createdAt,
+	currentUserName,
+	replies,
+}) {
 	return (
-		<div className="comment">
-			<p className="comment-score">{score}</p>
-			<div className="comment-main">
-				<div className="comment-head">
-					<div className="avatar">
-						<img
-							src={user.image.png}
-							height="32"
-							width="32"
-							alt={user.username}
-						/>
+		<div className="comment-container">
+			<div className="comment">
+				<p className="comment-score">{score}</p>
+				<div className="comment-main">
+					<div className="comment-head">
+						<div className="avatar">
+							<img
+								src={user.image.png}
+								height="32"
+								width="32"
+								alt={user.username}
+							/>
+						</div>
+						<p className="username">
+							{user.username}
+							{currentUserName == user.username && (
+								<span className="you">you</span>
+							)}
+						</p>
+						<p className="createdAt">{createdAt}</p>
+						<button className="button-secondary">
+							<img
+								src="/icon-reply.svg"
+								alt="reply"
+								height="12px"
+								width="12px"
+							/>
+							reply
+						</button>
 					</div>
-					<p className="username">
-						{user.username}
-						{currentUserName == user.username && (
-							<span className="you">you</span>
-						)}
-					</p>
-					<p className="createdAt">{createdAt}</p>
-					<button className="button-secondary">
-						<img
-							src="/icon-reply.svg"
-							alt="reply"
-							height="12px"
-							width="12px"
-						/>
-						reply
-					</button>
+					<p className="content">{content}</p>
 				</div>
-				<p className="content">{content}</p>
 			</div>
+			{replies?.length > 0 && (
+				<div className="replies-container">
+					<div className="line"></div>
+					<div className="replies">
+						{replies.map((reply) => (
+							<Comment
+								key={reply.id}
+								{...reply}
+								currentUserName={user.username}
+							/>
+						))}
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
