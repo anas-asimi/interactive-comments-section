7151f2f0b55ff8220620be552e13cc5b37d81981
Anas Asimi <anas2020cr7@gmail.com>
2023-08-26 22:25:59 +0100

    complete comments reducers

--- src/redux/commentsReducers.js
+++ src/redux/commentsReducers.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // add new comment
 function addComment(state, action) {
 	console.log(action);
@@ -10,25 +11,30 @@ function addComment(state, action) {
 	}
 	state.totalComments++;
 }
+//
+//
 // update old comment
 function updateComment(state, action) {
 	console.log(action);
+	let content = action.payload.content;
 	let target = findByPath(state.comments, action.payload.path);
-	target.assign(action.payload);
+	target.content = content
 }
+//
+//
 // remove old comment
 function removeComment(state, action) {
-	console.log(action);
+	let nodeid = action.payload.path.slice(-1);
 	if (action.payload.path.length == 1) {
-		delete state.comments[action.payload.path];
+		state.comments = state.comments.filter((ele) => ele.id != nodeid);
 	} else {
-		let targetId = action.payload.path.slice(-1);
 		let parentPath = action.payload.path.slice(0, -1);
 		let parentNode = findByPath(state.comments, parentPath);
 		parentNode.replies = parentNode.replies.filter(
-			(ele) => ele.id != targetId
+			(ele) => ele.id != nodeid
 		);
 	}
+	state.totalComments--;
 }
 
 export default {
@@ -37,14 +43,14 @@ export default {
 	updateComment,
 };
 
+//
+//
 // find comment reference by path
 export function findByPath(tree, path) {
 	if (path == "") return tree;
 	let nodeId = path.slice(0, 1);
 	path = path.slice(1);
 	let node = tree.find((ele) => ele.id == nodeId);
-	if (path == "") {
-		return node;
-	}
+	if (path == "") return node;
 	return findByPath(node.replies, path);
 }
