68565cf9d1bff447f021877595cbcefe93d212ca
Anas Asimi <anas2020cr7@gmail.com>
2023-08-27 14:40:37 +0100

    complete submit new comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Comment from "./components/Comment.jsx";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import initializedata from "./utils.js";
+import { addComment } from "./redux/comments.js";
 import "./App.css";
 
 function App() {
 	const { currentUser } = useSelector((state) => state.userSlice);
 	const { comments } = useSelector((state) => state.commentsSlice);
-	// console.log(comments);
+	const dispatch = useDispatch();
+	// state represent your comment target (normal comment or reply to someone)
+	const [replyingTo, setReplyingTo] = useState();
+	const commentField = useRef();
 	useEffect(initializedata, []);
-
+	function submit(e) {
+		e.preventDefault();
+		if (commentField.current.value) {
+			dispatch(
+				addComment({
+					content: commentField.current.value,
+					parentPath: replyingTo?.path ?? "",
+					user: { ...currentUser },
+				})
+			);
+			commentField.current.value = "";
+		} else {
+			console.log("empty comment !!");
+		}
+	}
+	function hanldeReplyingTo(path,username) {
+		setReplyingTo({
+			path,
+			username,
+		});
+		commentField.current.focus();
+	}
 	return (
 		<>
 			<section className="comment-section">
 				<div className="comments">
 					{comments &&
 						comments.map((comment) => (
-							<Comment key={comment.id} {...comment} />
+							<Comment
+								key={comment.id}
+								{...comment}
+								setReplyingTo={hanldeReplyingTo}
+							/>
 						))}
 				</div>
 				{currentUser && (
@@ -36,10 +65,32 @@ function App() {
 							label="add-comment"
 							title="add-comment"
 							rows="4"
+							ref={commentField}
+							placeholder={
+								replyingTo?.username
+									? `replying to ${replyingTo.username}`
+									: ""
+							}
 						></textarea>
-						<button type="submit" className="button-primary">
-							send
-						</button>
+						<div className="button-group">
+							<button
+								type="submit"
+								className="button-primary"
+								onClick={submit}
+							>
+								send
+							</button>
+							<button
+								className="button-primary"
+								onClick={(e) => {
+									e.preventDefault();
+									setReplyingTo();
+								}}
+								disabled={!replyingTo}
+							>
+								reset
+							</button>
+						</div>
 					</form>
 				)}
 			</section>
